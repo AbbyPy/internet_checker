55f29d8789106045129b5087c3f94fe324a714b2
Giorgio Abbadessa <gio.abbadessa@gmail.com>
2020-12-24 14:37:40 +0100

    eliminate la funzione di testing da daemon.py

--- daemon.py
+++ daemon.py
@@ -7,15 +7,16 @@ while True:
     sleep(delay)
 
     status = checker.check()
-    if status: print("ONLINE")
+    #if status: print("ONLINE")
     if status is False:
         start_day = strftime("%d/%m/%Y")
         start_time = strftime("%H:%M:%S")
         while status is False:
             pass
         stop_time = strftime("%H:%M:%S")
-
-        f = open(start_day, "a")
+        
+        file_path = f"{start_day}.log"
+        f = open(file_path, "a")
         error_text = f"{start_time} -- {stop_time} : OFFLINE\n"
         f.write(error_text)
         f.close()
