29875fe6557ced0382c190e1110f3f23b72647f0
Giorgio Abbadessa <gio.abbadessa@gmail.com>
2020-12-24 14:35:23 +0100

    aggiunta funzione per la scrittura dei log

--- daemon.py
+++ daemon.py
@@ -1,5 +1,5 @@
 import checker
-from time import sleep
+from time import sleep, strftime
 
 delay = 10
 
@@ -7,4 +7,16 @@ while True:
     sleep(delay)
 
     status = checker.check()
-    
+    if status: print("ONLINE")
+    if status is False:
+        start_day = strftime("%d/%m/%Y")
+        start_time = strftime("%H:%M:%S")
+        while status is False:
+            pass
+        stop_time = strftime("%H:%M:%S")
+
+        f = open(start_day, "a")
+        error_text = f"{start_time} -- {stop_time} : OFFLINE\n"
+        f.write(error_text)
+        f.close()
+
