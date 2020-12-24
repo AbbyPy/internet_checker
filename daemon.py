62bebea2dd44eaefa8ec8d5e668536a4f5c7c583
Giorgio Abbadessa <gio.abbadessa@gmail.com>
2020-12-24 14:58:07 +0100

    aggiunte scritte per il debug

--- daemon.py
+++ daemon.py
@@ -1,22 +1,28 @@
 import checker
 from time import sleep, strftime
 
-delay = 10
+delay = 2
 
 while True:
     sleep(delay)
 
     status = checker.check()
-    #if status: print("ONLINE")
+    if status: print("ONLINE")
     if status is False:
-        start_day = strftime("%d/%m/%Y")
+        start_day = strftime("%d-%m-%Y")
         start_time = strftime("%H:%M:%S")
+        print("OFFLINE START")
+    
         while status is False:
-            pass
+            sleep(delay)
+            status = checker.check()
+            print("STILL OFFLINE")
+
         stop_time = strftime("%H:%M:%S")
+        print("OFFLINE STOP")
         
         file_path = f"{start_day}.log"
-        f = open(file_path, "a")
+        f = open(file_path, "a+")
         error_text = f"{start_time} -- {stop_time} : OFFLINE\n"
         f.write(error_text)
         f.close()
