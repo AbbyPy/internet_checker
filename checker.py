6fefb654793a363ea4879fa3a1c2e7ae397d9c9b
Giorgio Abbadessa <gio.abbadessa@gmail.com>
2020-12-24 14:43:18 +0100

    rimozione scrittura dell'errore

--- checker.py
+++ checker.py
@@ -8,5 +8,5 @@ def check(host="8.8.8.8", port=53, timeout=10):
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
         return True
     except socket.error as err:
-        print(err)
+        #print(err)
         return False
